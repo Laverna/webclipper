ab9012bf2e34e42dc66740ed0424c04eec010945
wwebfor <wwebfor@gmail.com>
2014-02-02 03:56:27 +0600

    to-markdown

---
+++ extension/chrome/content/js/webclipper.js
@@ -0,0 +1,74 @@
+/**
+ * Laverna web clipper
+ * @license GNU GPLv3
+ */
+var webclipper = function () {
+
+    return {
+        init: function () {
+            this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
+                 .getService(Components.interfaces.nsIPrefService)
+                 .getBranch("extensions.laverna-clipper.");
+
+            this.prefs.addObserver("", this, false);
+        },
+
+        clip: function () {
+            var title = content.document.getElementsByTagName('title'),
+                body = content.document.getElementsByTagName('body');
+
+            if (title.length === 0 || body.length === 0) {
+                return;
+            }
+
+            webclipper.redirect(body[0].innerHTML, title[0].innerHTML);
+        },
+
+        redirect: function (body, title) {
+            var myUrl = this.prefs.getCharPref("appURL") + '/index.html#/notes/add',
+                browser = top.document.getElementById('content'),
+                tab = browser.addTab(myUrl),
+                inter,
+                result;
+
+            // use this line to focus the new tab, otherwise it will open in background
+            browser.selectedTab = tab;
+
+            tab.addEventListener('load', function tabLoad() {
+                if (content.window.location.href === myUrl) {
+                    tab.removeEventListener('load', tabLoad, false);
+
+                    // Try to find Form's input in the DOM until success
+                    inter = setInterval(function () {
+                        result = webclipper.fillForm(body, title);
+                        if (result === true) {
+                            clearInterval(inter);
+                        }
+                    }, 1000);
+                }
+            }, false);
+        },
+
+        fillForm: function (body, title) {
+            var inputTitle = content.document.getElementById('inputTitle'),
+                inputText = content.document.getElementById('clipContent');
+
+            if ( !inputTitle || !inputText) {
+                return false;
+            }
+
+            body = toMarkdown(body);
+
+            inputTitle.value = title;
+            inputText.value = body.replace(/(<([^>]+)>)/ig, '');
+            inputTitle.blur();
+            return true;
+        }
+
+    };
+
+} ();
+
+window.addEventListener('load', function (e) {
+    webclipper.init();
+}, false);
---
+++ extension/defaults/preferences/defaults.js
@@ -0,0 +1,3 @@
+pref("extensions.laverna-clipper.appURL", "https://laverna.cc");
+pref("extensions.laverna-clipper.tags", "clipped");
+pref("extensions.laverna-clipper.hotKey", false);
