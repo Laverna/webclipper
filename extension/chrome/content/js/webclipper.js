3e1bef90a0cd636e78711337563e7cf84a1d9836
wwebfor <wwebfor@gmail.com>
2014-02-05 18:58:08 +0600

    strip html tags and whitespaces

--- extension/chrome/content/js/webclipper.js
+++ extension/chrome/content/js/webclipper.js
@@ -5,6 +5,9 @@
 var webclipper = function () {
 
     return {
+        /**
+         * Load settings of extension
+         */
         init: function () {
             this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
                  .getService(Components.interfaces.nsIPrefService)
@@ -13,10 +16,15 @@ var webclipper = function () {
             this.prefs.addObserver("", this, false);
         },
 
+        /**
+         * Clipper
+         */
         clip: function (type) {
             var title = content.document.getElementsByTagName('title'),
-                body = content.document.getElementsByTagName('body');
-if (title.length === 0 || body.length === 0) { return;
+                body = $('body', content.document);
+
+            if (title.length === 0 || body.length === 0) {
+                return;
             }
 
             // Clip selected item
@@ -37,17 +45,22 @@ if (title.length === 0 || body.length === 0) { return;
                         case 'click':
                             $els.off('mouseover mouseout click');
                             $content.css('outline', 'none');
-                            webclipper.redirect($content.html(), title[0].innerHTML);
+                            webclipper.redirect($content.clone(), title[0].innerHTML);
                             break;
                     }
                 });
             }
             // Or entire page
             else {
-                webclipper.redirect(body[0].innerHTML, title[0].innerHTML);
+                body = body.clone();
+                $('aside,#footer,#header,.header,nav').remove();
+                webclipper.redirect(body, title[0].innerHTML);
             }
         },
 
+        /**
+         * Redirect to Laverna app page
+         */
         redirect: function (body, title) {
             var myUrl = this.prefs.getCharPref("appURL") + '/index.html#/notes/add',
                 browser = top.document.getElementById('content'),
@@ -78,20 +91,48 @@ if (title.length === 0 || body.length === 0) { return;
 
         fillForm: function (body, title, url) {
             var inputTitle = $('#inputTitle', content.document),
-                inputText = $('#clipContent', content.document);
+                inputText = $('#clipContent', content.document),
+                needAuth = $('input[name=password]', content.document);
 
-            if (inputTitle.length === 0 || inputText.length === 0) {
+            if (inputTitle.length === 0 || inputText.length === 0 || needAuth.length !== 0) {
                 return false;
             }
 
-            link = "[Original url](" + url +") " + this.prefs.getCharPref("defaultTags")
-                    + "\n\n-----------\n\n";
+            // Remove some html tags
+            $('script,noscript,style,link,footer,form', body).remove();
+            $('#disqus,.comments,#comments', body).remove();
+            body = this.cleanHtml(body).html();
+
+            // Add link to original post
+            link = "[Original url](" + url +") " + this.prefs.getCharPref("defaultTags") +
+                    "\n\n-----------\n\n";
             body = link + toMarkdown(body);
+
+            // Strip html tags
+            body = String(body).replace(/<\/?[^>]+>/g, '');
 
             inputTitle.val(title);
             inputText.val(body);
             inputTitle.blur();
             return true;
+        },
+
+        /**
+         * Clear whitespace between html tags
+         */
+        cleanHtml: function (str) {
+            var that = this;
+            str.contents().filter(function() {
+                if (this.nodeType != 3) {
+                    that.cleanHtml($(this));
+                    return false;
+                }
+                else {
+                    this.textContent = $.trim(this.textContent);
+                    return !/\S/.test(this.nodeValue);
+                }
+            }).remove();
+            return str;
         }
 
     };
